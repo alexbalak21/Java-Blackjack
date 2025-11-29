dbadf1295bb081da86e79eaa8d13228c29c867c3
Alex Balak <alex.balak@outlook.com>
2025-11-29 20:47:31 +0100

    working panels

--- src/main/java/window/DealerPanel.java
+++ src/main/java/window/DealerPanel.java
@@ -1,5 +1,13 @@
 package window;
 
-public class DealerPanel {
+import javax.swing.*;
+import java.awt.*;
+
+public class DealerPanel extends JPanel {
     
+    public DealerPanel() {
+        setBackground(new Color(1, 127, 1)); // Match window background
+        setBorder(BorderFactory.createTitledBorder("Dealer"));
+        // Add dealer-specific components here
+    }
 }
